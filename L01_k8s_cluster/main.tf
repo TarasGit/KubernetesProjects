2eca8cc78b36ead0911d09fa49f0222ffb25a786
Taras Iks <taras@MacBook-Air-von-Taras.fritz.box>
2025-12-31 15:47:22 +0100

    Update path to deployment.yml

--- L01_k8s_cluster/main.tf
+++ L01_k8s_cluster/main.tf
@@ -42,6 +42,6 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_manifest" "echo_app" {
-manifest = yamldecode(file("${path.module}/KubernetesProjects/L01_k8s_cluster/deployment.yaml"))
+manifest = yamldecode(file("${path.module}/deployment.yml"))
   depends_on = [ azurerm_kubernetes_cluster.myaks ]
 }
