bb82501513a1a9d54df5005168318d7adb518492
Taras Iks <taras@MacBook-Air-von-Taras.fritz.box>
2025-12-31 15:33:09 +0100

    Add kubernetes cluster to terraform

--- L01_k8s_cluster/main.tf
+++ L01_k8s_cluster/main.tf
@@ -17,3 +17,32 @@ resource "azurerm_resource_group" "myk8s" {
   location = "westeurope"
 }
 
+resource "azurerm_kubernetes_cluster" "myaks" {
+  name = "mycluster"
+  location = azurerm_resource_group.myk8s.location
+  resource_group_name = azurerm_resource_group.myk8s.name
+  dns_prefix = "myk8s"
+
+  default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size = "Standard_D2s_V3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.myaks.kube_config.0.host
+  client_certificate = base64decode(azurerm_kubernetes_cluster.myaks.kube_config.0.client_certificate)
+  client_key = base64decode(azurerm_kubernetes_cluster.myaks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.myaks.kube_config.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_manifest" "echo_app" {
+  manifest = yamldecode(file("${path.module}/deployment.yaml"))
+
+  depends_on = [ azurerm_kubernetes_cluster.myaks ]
+}
