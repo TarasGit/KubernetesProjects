9ee668a78ed2ba702379cd38d4e850c3f8a18a48
Taras Iks <taras@MacBook-Air-von-Taras.fritz.box>
2025-12-31 15:48:33 +0100

    Update file

--- L01_k8s_cluster/main.tf
+++ L01_k8s_cluster/main.tf
@@ -1,32 +1,32 @@
+# 1. Provider Definition
 terraform {
   required_providers {
-    azurerm    = { source = "hashicorp/azurerm", version = "~> 3.0" }
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.0" }
-
   }
 }
 
 provider "azurerm" {
-  features {
-    
-  }
+  features {}
 }
 
-resource "azurerm_resource_group" "myk8s" {
-  name = "myk8sproject"
+# 2. Ressourcengruppe
+resource "azurerm_resource_group" "k8s" {
+  name     = "MyK8sProject"
   location = "westeurope"
 }
 
-resource "azurerm_kubernetes_cluster" "myaks" {
-  name = "mycluster"
-  location = azurerm_resource_group.myk8s.location
-  resource_group_name = azurerm_resource_group.myk8s.name
-  dns_prefix = "myk8s"
+# 3. AKS Cluster (mit deiner funktionierenden VM-Größe)
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "MyFirstCluster"
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+  dns_prefix          = "myk8s"
 
   default_node_pool {
-    name = "default"
+    name       = "default"
     node_count = 1
-    vm_size = "Standard_D2s_V3"
+    vm_size    = "Standard_D2s_v3" # Die Größe, die vorhin geklappt hat
   }
 
   identity {
@@ -34,14 +34,18 @@ resource "azurerm_kubernetes_cluster" "myaks" {
   }
 }
 
+# 4. Kubernetes Provider Konfiguration 
+# (Wichtig: Nutzt die Daten des gerade erstellten Clusters)
 provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.myaks.kube_config.0.host
-  client_certificate = base64decode(azurerm_kubernetes_cluster.myaks.kube_config.0.client_certificate)
-  client_key = base64decode(azurerm_kubernetes_cluster.myaks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.myaks.kube_config.0.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
 
+# 5. Das Deployment aus deiner YAML-Datei
 resource "kubernetes_manifest" "echo_app" {
-manifest = yamldecode(file("${path.module}/deployment.yml"))
-  depends_on = [ azurerm_kubernetes_cluster.myaks ]
+  manifest = yamldecode(file("${path.module}/deployment.yml"))
+
+  depends_on = [azurerm_kubernetes_cluster.aks]
 }
