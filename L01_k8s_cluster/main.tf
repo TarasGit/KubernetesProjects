9f6621e486e427f29f40ff3947a4ada6b4f48860
Taras Iks <taras@MacBook-Air-von-Taras.fritz.box>
2025-12-31 15:41:24 +0100

    Add relative path to deployment.yml

--- L01_k8s_cluster/main.tf
+++ L01_k8s_cluster/main.tf
@@ -42,6 +42,6 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_manifest" "echo_app" {
-manifest = yamldecode(file("${path.module}/deployment.yaml"))
+manifest = yamldecode(file("${path.module}/L01_k8s_cluster/deployment.yaml"))
   depends_on = [ azurerm_kubernetes_cluster.myaks ]
 }
